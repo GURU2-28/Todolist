d216348fcd2520a62e78d40784363451f8fe3c42
minj-L <wonder_minji@naver.com>
2021-02-03 21:55:23 +0900

    메뉴바 추가-성취율, 이전할일 조회

--- GURU2_Todolist/app/src/main/java/com/example/guru2_todolist/MainActivity.kt
+++ GURU2_Todolist/app/src/main/java/com/example/guru2_todolist/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.guru2_todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,4 +16,23 @@ class MainActivity : AppCompatActivity() {
     //주석 추가-예린
     
     //주석 추가-규리
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    //메뉴바를 클릭했을 때의 동작
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.action_achieve -> { //성취율 메뉴 동작
+                var intent = Intent(this, Achievement::class.java)
+                startActivity(intent)
+                return true
+            }
+
+            //R.id.past_todo -> {} //이전 할일 조회하기
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
