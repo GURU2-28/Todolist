0d38f2d8508b71d247f80d1c65f0e316f50749b5
minj-L <wonder_minji@naver.com>
2021-02-04 14:40:48 +0900

    이전 구현방식 삭제

--- GURU2_Todolist/app/src/main/java/com/example/guru2_todolist/MainActivity.kt
+++ GURU2_Todolist/app/src/main/java/com/example/guru2_todolist/MainActivity.kt
@@ -16,23 +16,4 @@ class MainActivity : AppCompatActivity() {
     //주석 추가-예린
     
     //주석 추가-규리
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    //메뉴바를 클릭했을 때의 동작
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item?.itemId){
-            R.id.action_achieve -> { //성취율 메뉴 동작
-                var intent = Intent(this, Achievement::class.java)
-                startActivity(intent)
-                return true
-            }
-
-            //R.id.past_todo -> {} //이전 할일 조회하기
-        }
-        return super.onOptionsItemSelected(item)
-    }
 }
