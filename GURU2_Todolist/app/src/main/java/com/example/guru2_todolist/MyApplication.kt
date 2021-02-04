052af2e5069dc76a1c1d45f6aaba93c96e36ad6f
yerin <wowmtjf@naver.com>
2021-02-04 15:52:20 +0900

    //데이터베이스 연동

--- GURU2_Todolist/app/src/main/java/com/example/guru2_todolist/MyApplication.kt
+++ GURU2_Todolist/app/src/main/java/com/example/guru2_todolist/MyApplication.kt
@@ -2,7 +2,7 @@ package com.example.guru2_todolist
 
 import android.app.Application
 import io.realm.Realm
-
+//Realm 데이터베이스 연동준비
 class MyApplication:Application() {
     override fun onCreate(){
         super.onCreate()
--- GURU2_Todolist/app/src/main/java/com/example/guru2_todolist/Todo.kt
+++ GURU2_Todolist/app/src/main/java/com/example/guru2_todolist/Todo.kt
@@ -1,8 +1,13 @@
 package com.example.guru2_todolist
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
+//데이터베이스 모델클래스 생성
+//클래스명은 테이블명과 동일
+
 open class Todo (
+    //기본키
     @PrimaryKey var id:Long=0
-    ,var title: String=""
-    ,var date:Long=0
+    ,var title: String=""   //제목
+    ,var date:Long=0    //날짜
+    ,var content:String=""
 ) :  RealmObject()
