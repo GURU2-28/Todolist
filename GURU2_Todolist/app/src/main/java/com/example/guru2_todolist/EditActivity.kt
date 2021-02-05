a05ef07f0302d82349d042adb37aad42be865b5d
minj-L <wonder_minji@naver.com>
2021-02-05 11:46:51 +0900

    //주석 설명 추가

--- GURU2_Todolist/app/src/main/java/com/example/guru2_todolist/EditActivity.kt
+++ GURU2_Todolist/app/src/main/java/com/example/guru2_todolist/EditActivity.kt
@@ -12,6 +12,7 @@ import java.util.*
 
 class EditActivity : AppCompatActivity() {
 
+    //realm 인스턴스 얻기
     val realm = Realm.getDefaultInstance()
     val calendar: Calendar = Calendar.getInstance()
 
@@ -19,6 +20,7 @@ class EditActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
 
+        // todo list 업데이트 조건
         val id = intent.getLongExtra("id", -1L)
         if(id == -1L){
             insertMode()
@@ -26,13 +28,16 @@ class EditActivity : AppCompatActivity() {
             updateMode(id)
         }
 
+        // 캘린더 뷰의 날짜를 선택했을 때 캘린더 객체의 설정
         calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
             calendar.set(Calendar.YEAR, year)
             calendar.set(Calendar.MONTH, month)
             calendar.set(Calendar.DAY_OF_YEAR, dayOfMonth)
         }
 
+        //ListView와 연결
         val realmResult = realm.where<Todo>().findAll().sort("date",Sort.DESCENDING)
+
         //todo_list
         val adapter = TodoListAdapter(realmResult)
         listView.adapter = adapter
@@ -40,6 +45,7 @@ class EditActivity : AppCompatActivity() {
         realmResult.addChangeListener { _ -> adapter.notifyDataSetChanged() }
     }
 
+    // 할일 추가 모드 초기화
     private fun insertMode(){
         deleteFab.visibility = View.GONE
 
@@ -48,6 +54,7 @@ class EditActivity : AppCompatActivity() {
         }
     }
 
+    // 수정모드 초기화
     private fun updateMode(id: Long){
         val todo = realm.where<Todo>().equalTo("id",id).findFirst()!!
         todoEditText.setText(todo.title)
@@ -57,6 +64,7 @@ class EditActivity : AppCompatActivity() {
             updateTodo(id)
         }
 
+        // 수정하면서 삭제버튼 누를 시 삭제
         deleteFab.setOnClickListener {
             deleteTodo(id)
         }
