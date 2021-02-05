9e3ec83a0e4a7c9f5924b0beb859b026aedf99b6
minj-L <wonder_minji@naver.com>
2021-02-05 18:14:59 +0900

    //gradle에 viewBinding buildFeatures 추가

---
+++ GURU2_Todolist/app/build/generated/source/kapt/debug/io/realm/com_example_guru2_todolist_TodoRealmProxyInterface.java
@@ -0,0 +1,13 @@
+package io.realm;
+
+
+public interface com_example_guru2_todolist_TodoRealmProxyInterface {
+    public long realmGet$id();
+    public void realmSet$id(long value);
+    public String realmGet$title();
+    public void realmSet$title(String value);
+    public long realmGet$date();
+    public void realmSet$date(long value);
+    public String realmGet$content();
+    public void realmSet$content(String value);
+}
