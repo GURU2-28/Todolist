576308dddfad919661a4abeafbb2cb2d24daa5ae
HyejooJung <doitforjung@gmail.com>
2021-02-06 11:37:54 +0900

    xml추가 및 수정 중 이미지파일 drawable에 복붙해주세요!

--- GURU2_Todolist/app/src/main/java/com/example/guru2_todolist/MainActivity.kt
+++ GURU2_Todolist/app/src/main/java/com/example/guru2_todolist/MainActivity.kt
@@ -3,6 +3,18 @@ package com.example.guru2_todolist
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_list.*
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Rect
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_header.view.*
+import kotlinx.android.synthetic.main.item_main.view.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,4 +25,25 @@ class MainActivity : AppCompatActivity() {
             setContentView(R.layout.item_todo)
         }
     }
+
+    /* 신규 항목 추가(리사이클러뷰에)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 10 && resultCode == Activity.RESULT_OK) {
+            // selectDB()
+        }
+    }*/
+
+    // 뷰를 준비함
+    class HeaderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val headerView = view.itemHeaderView
+    }
+
+    // 데이타 준비함
+    class DataViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val completedIconView = view.completedIconView
+        val itemTitleView = view.itemTitleView
+        val itemContentView = view.itemContentView
+    }
+
 }
